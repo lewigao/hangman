f7960d962d043cb24bd8431fa8c02bc29f3cbb66
lewigao <lewigao@gmail.com>
2025-10-20 12:59:19 -0400

    tweaked newGame logic

--- hangman.js
+++ hangman.js
@@ -1,18 +1,41 @@
 var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
-
-
 var word = "";
+var guesses = "";
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
 
+    updatePage();
+}
+
+function guessLetter(){
+    var input = document.getElementById("guess")
+    var letter = input.value;
+    guesses += letter;
+
+    updatePage();
+}
+
+function updatePage(){
     var clueString = "";
 
     for (var i = 0; i < word.length; i++){
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);    
+        if (guesses.indexOf(currentLetter) >= 0){
+            //guess right
+            clueString += currentLetter + " ";
+        }
+        else {
+            //guess wrong
+            clueString += "_ ";
+        }
     }
+    
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
-}
 
+    var guessArea = document.getElementById("usedletters");
+    guessArea.innterHTML = guesses;
+}
