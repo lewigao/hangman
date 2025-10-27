fe8115487ca07e114d984d87ce69995ce411c135
lewigao <lewigao@gmail.com>
2025-10-27 14:14:47 -0400

    added: disallows guessing before game start + clears input on guess

--- hangman.js
+++ hangman.js
@@ -17,17 +17,32 @@ function guessLetter() {
     var input = document.getElementById("guess")
     var letter = input.value;
 
-    if (word.indexOf(letter) < 0) { // look for the index of the guessed letter in our "word", if it's not: decrement
-        guess_count--;
+    var gameStarted = false;
+
+    // checks that a word has been chosen, which means a game has started
+    if (word == "") {
+        var usedLetter = document.getElementById("reminder");
+        usedLetter.innerHTML = "Please start a new game before trying to guess";
+    }
+    else if (word != "") {
+        gameStarted = true;
     }
 
-    if (guesses.includes(letter)) {
-        var usedLetter = document.getElementById("reminder");
-        usedLetter.innerHTML = `You've already guessed ${letter}, try another letter`;
-    }
-    else {
-        guesses += letter;
-        updatePage();
+    // only applies game logic if they start a new game
+    if (gameStarted === true) {
+        if (word.indexOf(letter) < 0) { // look for the index of the guessed letter in our "word", if it's not: decrement
+            guess_count--;
+        }
+
+        // disallows guessing the same letter
+        if (guesses.includes(letter)) {
+            var usedLetter = document.getElementById("reminder");
+            usedLetter.innerHTML = `You've already guessed "${letter}", try another letter`;
+        }
+        else {
+            guesses += letter;
+            updatePage();
+        }
     }
 }
 
@@ -61,4 +76,9 @@ function updatePage() {
     image.src = "images/hangman" + guess_count + ".gif"
     console.log(guess_count);
     console.log(image);
+
+    // updates the guess area
+    var input = document.getElementById("guess");
+    input.value = "";
+
 }
