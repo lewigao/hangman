da9c4d1fa516a629b9e9079d03480874019e7541
lewigao <lewigao@gmail.com>
2025-10-27 18:20:23 -0400

    added: winning/losing dialog in game area + clears input field on guess + misc fixes

--- hangman.js
+++ hangman.js
@@ -1,14 +1,21 @@
-var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+var POSSIBLE_WORDS = ["all"];
 var word = "";
 var guesses = "";
 MAX_GUESSES = 6;
 var guess_count = MAX_GUESSES;
+var spaced_word = "";
 
 function newGame() {
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
+    spaced_word = "";
+    document.getElementById("usedletters").style.color = "inherit";
+
     guess_count = MAX_GUESSES;
+    for (var i = 0; i < word.length; i++) {
+        spaced_word += word[i] + " ";
+    }
 
     updatePage();
 }
@@ -21,23 +28,25 @@ function guessLetter() {
 
     // checks that a word has been chosen, which means a game has started
     if (word == "") {
-        var usedLetter = document.getElementById("reminder");
-        usedLetter.innerHTML = "Please start a new game before trying to guess";
+        var userInfo = document.getElementById("reminder");
+        userInfo.innerHTML = "Please start a new game before trying to guess";
     }
     else if (word != "") {
         gameStarted = true;
     }
 
     // only applies game logic if they start a new game
-    if (gameStarted === true) {
+    if (gameStarted === true && guess_count > 0) {
+        var userInfo = document.getElementById("reminder");
+        var guess = document.getElementById("guess");
+
         if (word.indexOf(letter) < 0) { // look for the index of the guessed letter in our "word", if it's not: decrement
             guess_count--;
         }
 
-        // disallows guessing the same letter
+        // disallows guessing the same letter or blank
         if (guesses.includes(letter)) {
-            var usedLetter = document.getElementById("reminder");
-            usedLetter.innerHTML = `You've already guessed "${letter}", try another letter`;
+            userInfo.innerHTML = `You've already guessed "${letter}", try another letter`;
         }
         else {
             guesses += letter;
@@ -46,10 +55,19 @@ function guessLetter() {
     }
 }
 
+// self-explanatory
+function checkWin() {
+    if (spaced_word == document.getElementById("clue").innerHTML) {
+        guess_count = 0;
+        return true;
+    }
+    return false;
+}
+
 function updatePage() {
     var clueString = "";
-    var usedLetter = document.getElementById("reminder");
-    usedLetter.innerHTML = "";
+    var userInfo = document.getElementById("usedletters");
+    document.getElementById("reminder").innerHTML = "";
 
     for (var i = 0; i < word.length; i++) {
         var currentLetter = word.charAt(i);
@@ -81,4 +99,12 @@ function updatePage() {
     var input = document.getElementById("guess");
     input.value = "";
 
+        if (checkWin() == false && guess_count == 0) {
+            userInfo.style.color = "red";
+            userInfo.innerHTML = "Start a new game, you're done.";
+        }
+        else if (checkWin() == true) {
+            userInfo.style.color = "green";
+            userInfo.innerHTML = "You have won!";
+    }
 }
