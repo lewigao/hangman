6690cc74f25ed47a6331dcab22d7c6128c54b963
lewigao <lewigao@gmail.com>
2025-10-27 13:35:18 -0400

    added: duplicate answer detection

--- hangman.js
+++ hangman.js
@@ -4,7 +4,7 @@ var guesses = "";
 MAX_GUESSES = 6;
 var guess_count = MAX_GUESSES;
 
-function newGame(){
+function newGame() {
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
@@ -13,25 +13,32 @@ function newGame(){
     updatePage();
 }
 
-function guessLetter(){
+function guessLetter() {
     var input = document.getElementById("guess")
     var letter = input.value;
 
-    if (word.indexOf(letter) < 0){ // look for the index of the guessed letter in our "word", if it's not: decrement
+    if (word.indexOf(letter) < 0) { // look for the index of the guessed letter in our "word", if it's not: decrement
         guess_count--;
     }
 
-    guesses += letter;
-
-    updatePage();
+    if (guesses.includes(letter)) {
+        var usedLetter = document.getElementById("reminder");
+        usedLetter.innerHTML = `You've already guessed ${letter}, try another letter`;
+    }
+    else {
+        guesses += letter;
+        updatePage();
+    }
 }
 
-function updatePage(){
+function updatePage() {
     var clueString = "";
+    var usedLetter = document.getElementById("reminder");
+    usedLetter.innerHTML = "";
 
-    for (var i = 0; i < word.length; i++){
-        var currentLetter = word.charAt(i);    
-        if (guesses.indexOf(currentLetter) >= 0){
+    for (var i = 0; i < word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) {
             //guess right
             clueString += currentLetter + " ";
         }
@@ -40,7 +47,7 @@ function updatePage(){
             clueString += "_ ";
         }
     }
-    
+
     // updates clue string
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
